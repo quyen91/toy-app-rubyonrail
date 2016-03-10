d78b2542f1f4549b214c66a2903f66def0d6c77d
quyen91 <chibedethuong91@gmail.com>
2016-03-10 11:57:29 +0700

    fill layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+ root 'static_page#home'
+ get 'help' => 'static_pages#help'
+ get 'about' => 'static_pages#about'
+ get 'contact' => 'static_pages#contact'
+ get 'signup'  => 'users#new'
+
+
+  resources :microposts
+  resources :users
   get 'home/hello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#hello'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
