889a5775d87fb7ded75c2c7f6e0162043924a93a
quyen91 <chibedethuong91@gmail.com>
2016-03-12 17:15:21 +0700

    login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+  
   def create
   	user = User.find_by(email:params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
-  		redirect_to user
+      remember user
+  		redirect_to current_user
 
   	else
   		flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
   		render 'new'
   end
 
-  def destroy
-  end
 end
+
+  def destroy
+    logout
+    redirect_to root_path
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311075417) do
+ActiveRecord::Schema.define(version: 20160312070220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160311075417) do
     t.datetime "updated_at",      null: false
     t.string   "password"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
